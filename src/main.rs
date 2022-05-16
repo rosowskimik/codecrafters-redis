69d1f702cde205be28830b22ec780fa538089ee9
Mikołaj Rosowski <m.rosowski1@wp.pl>
2022-05-17 01:46:51 +0200

    3rd

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,57 @@
-#[allow(unused_imports)]
-use std::env;
-use std::fmt::{self, Display, Formatter};
-#[allow(unused_imports)]
-use std::fs;
-use std::io::BufRead;
+mod resp;
 
-use bytes::Bytes;
-use tokio::io::AsyncWriteExt;
-use tokio::net::TcpListener;
+use bytes::{Buf, BytesMut};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    stream::StreamExt,
+};
 
-#[derive(Debug, PartialEq, PartialOrd)]
-#[allow(dead_code)]
-enum RespValue {
-    SimpleString(String),
-    Error(String),
-    Integer(i64),
-    BulkString(String),
-    Array(Vec<RespValue>),
-    Null,
-}
-
-#[allow(dead_code)]
-impl RespValue {
-    fn try_from_reader(mut r: impl BufRead) -> Result<Self, &'static str> {
-        let mut first_char: [u8; 1] = [0];
-
-        r.read_exact(&mut first_char).unwrap();
-
-        match first_char[0] as char {
-            '+' => {
-                let mut buffer = String::new();
-                r.read_line(&mut buffer).unwrap();
-                Ok(RespValue::SimpleString(buffer))
-            }
-            '-' => {
-                let mut buffer = String::new();
-                r.read_line(&mut buffer).unwrap();
-                Ok(RespValue::Error(buffer))
-            }
-            ':' => {
-                let mut buffer = String::new();
-                r.read_line(&mut buffer).unwrap();
-                Ok(RespValue::Integer(buffer.parse().unwrap()))
-            }
-            '$' => {
-                let mut buffer = String::new();
-                r.read_line(&mut buffer).unwrap();
-
-                let size = buffer.parse().unwrap();
-                let mut buffer = Vec::with_capacity(size);
-
-                r.read_exact(&mut buffer).unwrap();
-
-                Ok(RespValue::BulkString(String::from_utf8(buffer).unwrap()))
-            }
-            '*' => {
-                let mut buffer = String::new();
-                r.read_line(&mut buffer).unwrap();
-
-                let size = buffer.parse().unwrap();
-                let mut values = Vec::with_capacity(size);
-
-                for _ in 0..size {
-                    values.push(RespValue::try_from_reader(&mut r)?);
-                }
-
-                Ok(RespValue::Array(values))
-            }
-            _ => Err("Something went wrong"),
-        }
-    }
-    fn as_bytes(&self) -> Bytes {
-        Bytes::copy_from_slice(self.to_string().as_bytes())
-    }
-}
-
-impl Display for RespValue {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::SimpleString(s) => write!(f, "+{}\r\n", s),
-            Self::Error(e) => write!(f, "-{}\r\n", e),
-            Self::Integer(i) => write!(f, ":{}\r\n", i),
-            Self::BulkString(s) => write!(f, "${}\r\n{}\r\n", s.len(), s),
-            Self::Array(v) => {
-                write!(f, "*{}\r\n", v.len())?;
-                for resp in v {
-                    write!(f, "{}", resp)?;
-                }
-                write!(f, "\r\n")
-            }
-            Self::Null => write!(f, "$-1\r\n"),
-        }
-    }
-}
+use resp::{RespError, RespValue};
 
 #[tokio::main]
 async fn main() {
     let mut listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    match listener.accept().await {
-        Ok((mut socket, _addr)) => {
-            socket
-                .write_buf(&mut RespValue::SimpleString("PONG".to_string()).as_bytes())
-                .await
-                .unwrap();
-        }
-        Err(e) => panic!("Something went wrong\n{}", e),
+    let mut incoming = listener.incoming();
+
+    while let Some(stream) = incoming.next().await {
+        let stream = stream.unwrap();
+        tokio::spawn(async move { handle_connection(stream).await });
     }
 }
 
-// async fn handle_message(msg: impl AsyncReadExt + std::pin::) {
-//     let mut buffer = BytesMut::with_capacity(4);
-//     let data = msg.read_exact(&mut buffer);
-// }
+async fn handle_connection(mut stream: TcpStream) {
+    loop {
+        let mut data = BytesMut::new();
+        let val = loop {
+            stream.read_buf(&mut data).await.unwrap();
+
+            match RespValue::try_from_bytes(&mut data) {
+                Ok(r) => break r,
+                Err(e) => {
+                    if !matches!(e, RespError::Incomplete) {
+                        panic!("Somethin went wrong");
+                    }
+                }
+            }
+        };
+
+        handle_command(val, &mut stream).await;
+    }
+}
+
+async fn handle_command(val: RespValue, stream: &mut TcpStream) {
+    if let RespValue::Array(val) = val {
+        let (command, _data) = val.split_at(1);
+        let _command = &command[0];
+        // match command[0] {
+
+        // }
+        let mut response = RespValue::Simple("PONG".to_string()).raw_bytes();
+        while response.has_remaining() {
+            stream.write_buf(&mut response).await.unwrap();
+        }
+    } else {
+        panic!("Sometgin went wrong");
+    }
+}
---
+++ src/resp.rs
@@ -0,0 +1,195 @@
+use std::fmt::{self, Display, Formatter};
+
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum RespValue {
+    Simple(String),
+    Error(String),
+    Integer(u64),
+    Bulk(String),
+    Null,
+    Array(Vec<RespValue>),
+}
+
+#[derive(Debug)]
+pub enum RespError {
+    Incomplete,
+    Other,
+}
+
+impl RespValue {
+    pub fn try_from_bytes(bytes: &mut BytesMut) -> Result<Self, RespError> {
+        if !bytes.has_remaining() {
+            return Err(RespError::Incomplete);
+        }
+
+        match bytes[0] {
+            b'+' => {
+                let line = read_line(bytes)?;
+                Ok(RespValue::Simple(line))
+            }
+            b'-' => {
+                let line = read_line(bytes)?;
+                Ok(RespValue::Error(line))
+            }
+            b':' => {
+                let line = read_line(bytes)?;
+                Ok(RespValue::Integer(line.parse().unwrap()))
+            }
+            b'$' => {
+                if bytes.starts_with(b"$-1\r\n") {
+                    bytes.advance(5);
+                    return Ok(RespValue::Null);
+                }
+
+                let line = peek_line(bytes)?;
+                let size_len = line.len();
+                let size = line.parse().unwrap();
+
+                if bytes.remaining() < line.len() + size + 5 {
+                    return Err(RespError::Incomplete);
+                }
+                bytes.advance(size_len + 3);
+
+                let bs = String::from_utf8((&bytes[..size]).to_vec()).unwrap();
+                bytes.advance(size + 2);
+
+                Ok(RespValue::Bulk(bs))
+            }
+            b'*' => {
+                if bytes.starts_with(b"*-1\r\n") {
+                    bytes.advance(5);
+                    return Ok(RespValue::Null);
+                }
+                // todo!()
+                let line = peek_line(bytes)?;
+                let size = line.parse().unwrap();
+                let mut inner = Vec::with_capacity(size);
+                let mut inner_size = 0;
+
+                {
+                    let mut value = bytes.clone();
+                    value.advance(line.as_bytes().len() + 3);
+                    for _ in 0..size {
+                        let len = value.len();
+                        inner.push(RespValue::try_from_bytes(&mut value)?);
+                        inner_size += len - value.len();
+                    }
+                }
+
+                bytes.advance(inner_size + 1);
+                Ok(RespValue::Array(inner))
+            }
+            _ => Err(RespError::Other),
+        }
+    }
+
+    pub fn raw_bytes(&self) -> Bytes {
+        let mut buf = match self {
+            Self::Simple(s) => {
+                let val = s.as_bytes();
+                let mut buf = BytesMut::with_capacity(val.len() + 3);
+                buf.put_u8(b'+');
+                buf.put(val);
+                buf
+            }
+            Self::Error(e) => {
+                let val = e.as_bytes();
+                let mut buf = BytesMut::with_capacity(val.len() + 3);
+                buf.put_u8(b'-');
+                buf.put(val);
+                buf
+            }
+            Self::Integer(i) => {
+                let val = i.to_string();
+                let val = val.as_bytes();
+                let mut buf = BytesMut::with_capacity(val.len() + 3);
+                buf.put_u8(b':');
+                buf.put(val);
+                buf
+            }
+            Self::Bulk(s) => {
+                let val = s.as_bytes();
+                let len = val.len();
+                let len_bytes = len.to_string();
+                let len_bytes = len_bytes.as_bytes();
+                let mut buf = BytesMut::with_capacity(len + len_bytes.len() + 3);
+
+                buf.put_u8(b'$');
+                buf.put(len_bytes);
+                buf.put(val);
+                buf
+            }
+            Self::Array(v) => {
+                let len = v.len();
+                let len_bytes = len.to_string();
+                let len_bytes = len_bytes.as_bytes();
+                let val = v.iter().map(|resp| resp.raw_bytes()).fold(
+                    BytesMut::new(),
+                    |mut acc, bytes| {
+                        acc.put(bytes);
+                        acc
+                    },
+                );
+                let mut buf = BytesMut::with_capacity(val.len() + len_bytes.len() + 3);
+                buf.put_u8(b'*');
+                buf.put_slice(&val);
+                buf
+            }
+            Self::Null => {
+                let mut buf = BytesMut::with_capacity(5);
+                buf.put_slice(b"$-1");
+                buf
+            }
+        };
+        buf.put_slice(b"\r\n");
+
+        buf.freeze()
+    }
+
+    pub fn array_push(&mut self, val: RespValue) -> Result<(), RespError> {
+        assert!(matches!(*self, Self::Array(_)));
+        if let Self::Array(v) = self {
+            v.push(val);
+            Ok(())
+        } else {
+            Err(RespError::Other)
+        }
+    }
+}
+
+fn peek_line(buf: &BytesMut) -> Result<&str, RespError> {
+    let start = 1;
+    let end = buf.len() - 1;
+    for i in start..end {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            return Ok(std::str::from_utf8(&buf[1..i]).unwrap());
+        }
+    }
+    Err(RespError::Incomplete)
+}
+
+fn read_line(buf: &mut BytesMut) -> Result<String, RespError> {
+    let start = 1;
+    let end = buf.len() - 1;
+    for i in start..end {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            let val = String::from_utf8((&buf[1..i]).to_vec()).unwrap();
+            buf.advance(i + 2);
+            return Ok(val);
+        }
+    }
+    Err(RespError::Incomplete)
+}
+
+impl std::error::Error for RespError {}
+
+impl Display for RespError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Incomplete => "Stream ended early".fmt(f),
+            Self::Other => "Something went wrong".fmt(f),
+        }
+    }
+}
